1e9c1028d49a6c8091ee5d313233a1638ee902f6
Aakash Kumar Gupta <aakashgupta18052003@gmail.com>
2024-07-22 01:46:24 +0530

    logic_gates_tb.v

---
+++ logic_gates_tb.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+
+
+module gate_tb();
+reg a,b;
+wire y0,y1,y2;
+gates dut(a,b,y0,y1,y2);
+
+initial 
+begin
+    a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+    #10 $stop;
+end 
+    
+endmodule
